7b4c3d5800f098df62904adec0ec79ed6e1ac3e8
Natan Rodrigues <67937025+Nata4n@users.noreply.github.com>
2022-03-15 15:37:48 -0300

    Fix QR Code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
+import * as fs from 'fs'
 import { Client } from './client/Client';
 import { fetchLatestBaileysVersion, useSingleFileAuthState } from "@adiwajshing/baileys";
 
-const { state } = useSingleFileAuthState('./src/sessions/sessions.json')
-
 const runBot = async () => {
+
+    /*
+        these lines are optional, 
+        they will create the folder 
+        where you will store your session, 
+        if you don't want you can delete it 
+        or change the path
+    */
+
+    const path = './src/sessions'
+        
+    fs.access(path, (error) => {
+        if (error) {
+            fs.mkdir(path, (error) => {
+                if (!error) {
+                    console.log('sessions folder created successfully')
+                }
+            })
+        }
+    })
+    
+    const { state } = useSingleFileAuthState(`${path}/sessions.json`)
     const { version } = await fetchLatestBaileysVersion()
     
     const client = new Client({
